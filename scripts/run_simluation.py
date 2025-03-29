50baabe47f0b4bd52c72a300746cda71b619aa94
JoanMarie4 <jleone5@binghamton.edu>
2025-03-29 17:26:41 -0400

    began environment setup

---
+++ scripts/run_simluation.py
@@ -0,0 +1,51 @@
+
+
+import argparse
+from isaaclab.app import AppLauncher
+
+#from isaaclab.robots.unitree import G1_CFG  # Import G1 config
+
+# create argparser
+parser = argparse.ArgumentParser(description="Tutorial on spawning prims into the scene.")
+# append AppLauncher cli args
+AppLauncher.add_app_launcher_args(parser)
+# parse the arguments
+args_cli = parser.parse_args()
+# launch omniverse app
+app_launcher = AppLauncher(args_cli)
+simulation_app = app_launcher.app
+
+import isaacsim.core.utils.prims as prim_utils
+
+from environment import design_scene
+
+import isaaclab.sim as sim_utils
+
+def main():
+    """Main function."""
+
+    # Initialize the simulation context
+    sim_cfg = sim_utils.SimulationCfg(dt=0.01, device=args_cli.device)
+    sim = sim_utils.SimulationContext(sim_cfg)
+    # Set main camera
+    sim.set_camera_view([2.0, 0.0, 2.5], [-0.5, 0.0, 0.5])
+
+    # Design scene by adding assets to it
+    design_scene()
+
+    # Play the simulator
+    sim.reset()
+    # Now we are ready!
+    print("[INFO]: Setup complete...")
+
+    # Simulate physics
+    while simulation_app.is_running():
+        # perform step
+        sim.step()
+
+
+if __name__ == "__main__":
+    # run the main function
+    main()
+    # close sim app
+    simulation_app.close()
