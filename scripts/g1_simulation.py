a172f08d25f11d023cc941cef9c3d25e18190cce
JoanMarie4 <jleone5@binghamton.edu>
2025-05-09 21:08:38 -0400

    final commit

--- scripts/g1_simulation.py
+++ scripts/g1_simulation.py
@@ -7,9 +7,9 @@ import scripts.reinforcement_learning.rsl_rl.cli_args as cli_args  # isort: skip
 from isaaclab.app import AppLauncher
 # add argparse arguments
 parser = argparse.ArgumentParser(
-    description="Test G1 Demo in rough terrain environment."
+    description="3D MR-NAMO Sim (in progress)"
 )
-from namosim.simulator import Simulator, create_sim_from_file
+from namosim.simulator import create_sim_from_file
 # append RSL-RL cli argumentsks
 cli_args.add_rsl_rl_args(parser)
 # append AppLauncher cli args
@@ -37,30 +37,30 @@ from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlVecEnvWrapper
 scenarios_folder = os.path.join(os.path.dirname(__file__), "../scenarios")
 from pxr import UsdGeom, Gf
 from flat_env_namo_cfg import G1FlatMultiRobotEnvCfg
-from namosim.world.entity import Entity, Movability, Style
+from namosim.world.entity import Movability
 import math
 import numpy as np
 from namosim.world.obstacle import Obstacle
-import isaacsim.core.utils.prims as prim_utils
-import isaacsim.core.prims as prim
-from isaaclab.sim import UsdFileCfg, PreviewSurfaceCfg
 import isaaclab.sim as sim_utils
 
 TASK = "Isaac-Velocity-Flat-G1-v0"
 RL_LIBRARY = "rsl_rl"
 
-class G1RoughDemo:
+class G1MrNavDemo:
         
     def __init__(self):
         """Initializes environment config designed for the interactive model and sets up the environment,
         loads pre-trained checkpoints, and registers keyboard events."""
+        """MY ADDITIONS: Assigned related navigation fields for MR-NAMO simulation"""
         agent_cfg: RslRlOnPolicyRunnerCfg = cli_args.parse_rsl_rl_cfg(TASK, args_cli)
         # load the trained jit policy
         checkpoint = get_published_pretrained_checkpoint(RL_LIBRARY, TASK)
         # create envionrment
+        """Change SVG filename to load different files"""
         simulation_file_path=os.path.join(
             scenarios_folder, "1_robot_around_obstacle.svg"
         )
+        #new fields
         self.sim = create_sim_from_file(simulation_file_path)
         self.world = self.sim.ref_world
         self.agents = self.world.agents
@@ -68,15 +68,17 @@ class G1RoughDemo:
         self.binary_grid = self.world.map
         self.cell_size = self.binary_grid.cell_size
         self.spawn_pos = self.compute_spawn_poses(self.agents, self.binary_grid)
+        self.scale= 1
+        self.step_idx = 0
+        self.history = {}
+        self.dynamic_entities = self.world.dynamic_entities
+
         env_cfg = G1FlatMultiRobotEnvCfg()
         env_cfg.scene.num_envs = len(self.agents)
         env_cfg.curriculum = None 
         env_cfg.commands.base_velocity.ranges.lin_vel_x = (0.0, 1.0)
         env_cfg.commands.base_velocity.ranges.heading = (-1.0, 1.0)
-        self.scale= 1.1
-        self.step_idx = 0
-        self.history = {}
-        self.dynamic_entities = self.world.dynamic_entities
+
         # wrap around environment for rsl-rl
         self.env = RslRlVecEnvWrapper(ManagerBasedRLEnv(cfg=env_cfg))
         self.device = self.env.unwrapped.device
@@ -90,12 +92,15 @@ class G1RoughDemo:
         self.create_camera()
         self.generate_3d_map_from_occupancy_grid(self.binary_grid)
         self.commands = torch.zeros(env_cfg.scene.num_envs, 4, device=self.device)
-        #self.commands[:, 0:3] = self.env.unwrapped.command_manager.get_command("base_velocity")
         self.set_up_keyboard()
         self._prim_selection = omni.usd.get_context().get_selection()
         self._selected_id = None
         self._previous_selected_id = None
         self._camera_local_transform = torch.tensor([-2.5, 0.0, 0.8], device=self.device)
+
+    '''
+    MAP WALL CREATION
+    '''
 
     def generate_3d_map_from_occupancy_grid(self, binary_grid):
         """Creates 3D obstacles from the binary occupancy grid."""
@@ -112,7 +117,7 @@ class G1RoughDemo:
     def create_boundary_walls(self, stage, binary_grid):
         """Creates 4 surrounding boundary walls around the map."""
         cell_size = binary_grid.cell_size
-        scale = 1.1
+        scale = 1
         width = binary_grid.d_width * cell_size * scale
         height = binary_grid.d_height * cell_size * scale
         wall_thickness = cell_size * scale
@@ -143,7 +148,7 @@ class G1RoughDemo:
 
     def create_box_at_cell(self, stage, prim_path, x, y, binary_grid):
         """Creates a scaled cube obstacle in the world at the given grid cell."""
-        scale = 1.1
+        scale = 1
         world_x = (x * binary_grid.cell_size + binary_grid.cell_size / 2 + binary_grid.grid_pose[0]) * scale
         world_y = (y * binary_grid.cell_size + binary_grid.cell_size / 2 + binary_grid.grid_pose[1]) * scale
         wall_height = binary_grid.cell_size * 20  # Taller than 1 cell
@@ -155,11 +160,14 @@ class G1RoughDemo:
         xform.SetTranslate(Gf.Vec3d(world_x, world_y, wall_height / 2))
         xform.SetScale(Gf.Vec3f(scale, scale, wall_height / binary_grid.cell_size))
 
-
+    '''
+    ROBOT SPAWN CREATION
+    '''
 
     def compute_spawn_poses(self, agents, binary_grid):
+        """Translates spawn poses from 2D sim to Isaac Sim"""
         spawn_poses = []
-        scale = 1.1
+        scale = 1
         for agent in agents.values():
             x = agent.pose[0] / binary_grid.cell_size
             y = agent.pose[1] / binary_grid.cell_size
@@ -170,22 +178,24 @@ class G1RoughDemo:
         return spawn_poses
 
     def set_agent_spawns(self, spawn_positions):
-
+        """puts each agent at its assigned spawn position"""
         robot = self.env.unwrapped.scene["robot"]
         device = robot.data.root_pos_w.device
-
         if len(spawn_positions) != self.env.num_envs:
             raise ValueError(
                 f"Number of spawn positions ({len(spawn_positions)}) does not match number of environments ({self.env.num_envs})."
             )
-
         for env_index, pos in enumerate(spawn_positions):
             pos_tensor = torch.tensor(pos, dtype=torch.float, device=device)
             robot.data.root_pos_w[env_index] = pos_tensor
-
         robot.write_root_state_to_sim(robot.data.root_state_w)
 
+    '''
+    UNMOVABLE OBSTACLE SPAWN CREATION
+    '''
+
     def convert_movable_obstacles(self):
+        """remakes movable obstacle to be unmovable and spawn object on map"""
         for entity_id, entity in self.dynamic_entities.items():
             if isinstance(entity, Obstacle):
                 # 1. Mark it unmovable
@@ -204,7 +214,7 @@ class G1RoughDemo:
                     pose=pose,
                     style=style,
                     movability=Movability.STATIC,
-                    full_geometry_acquired=True,
+                    full_geometry_acquired=True,    
                 )
 
                 # Add it to the world or map (depends on your setup)
@@ -212,7 +222,7 @@ class G1RoughDemo:
                 print(f"Placed replacement obstacle at {pose}.")        
         
     def add_obstacle(self, pose):
-        scale = 1.1
+        scale = 1
         x = pose[0] / self.binary_grid.cell_size
         y = pose[1] / self.binary_grid.cell_size
         world_x = (x * self.binary_grid.cell_size + self.binary_grid.cell_size / 2 + self.binary_grid.grid_pose[0]) * scale
@@ -221,7 +231,7 @@ class G1RoughDemo:
         table_prim_path = f"/World/Obstacles/thor_table_{int(world_x*100)}_{int(world_y*100)}"
 
         cfg_cuboid = sim_utils.MeshCuboidCfg(
-            size=(1, 1, 1),  # X, Y, Z dimensions in meters
+            size=(.75, .75, .75),  # X, Y, Z dimensions in meters
             visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.0, 0.0, 1.0)),
         )
 
@@ -231,8 +241,12 @@ class G1RoughDemo:
             translation=(world_x, world_y, 0.25),  # 0.25 puts the cube's base on the floor
         )
 
+    '''
+    CAMERA CREATION
+    '''
+
     def create_camera(self):
-        """Creates a camera to be used for third-person overhead view."""
+        """Creates an overhead camera"""
         stage = omni.usd.get_context().get_stage()
         self.viewport = get_viewport_from_window_name("Viewport")
 
@@ -246,8 +260,13 @@ class G1RoughDemo:
         UsdGeom.Xformable(camera_prim).AddRotateXYZOp().Set(Gf.Vec3f(9.43699, 0.0, -0.63826))
         self.viewport.set_active_camera(self.perspective_path)
 
+    '''
+    KEYBOARD INPUTS
+    '''
+
     def set_up_keyboard(self):
         """Sets up interface for keyboard input and registers the desired keys for control."""
+        """FROM h1.locomotion.py, used as reference and when needed in making implementation"""
         self._input = carb.input.acquire_input_interface()
         self._keyboard = omni.appwindow.get_default_app_window().get_keyboard()
         self._sub_keyboard = self._input.subscribe_to_keyboard_events(self._keyboard, self._on_keyboard_event)
@@ -278,9 +297,9 @@ class G1RoughDemo:
                         self.viewport.set_active_camera(self.perspective_path)
                     else:
                         self.viewport.set_active_camera(self.camera_path)
-            elif event.input.name == "G":
+            elif event.input.name == "G": #puts robots back in spawn position (finicky)
                 self.set_agent_spawns(self.spawn_pos)
-            elif event.input.name == "S":
+            elif event.input.name == "S": #used when testing timing for each velocity command
                 step = self.history[self.step_idx]
                 action_results = step.action_results
                 for agent_id, action_result in action_results.items():
@@ -293,10 +312,9 @@ class G1RoughDemo:
                         new_pose = action_result.robot_pose
                         cur_pose = self.agents[agent_id].pose
                         self.move_robot_for_duration(cur_pose, new_pose, agent_id, duration_steps=60)
-                        # now you can use robot_pose + env_index however you like
                         print(f"[env {env_index}] {agent_id} pose = {new_pose}")
                     else:
-                        # it's some other ActionResult (e.g. a failure or no-op), skip or handle differently
+                        #not a successfull action result (no robot adjustment currently)
                         print(f"[env {env_index}] {agent_id} did not succeed, skipping pose extraction")
                 self.step_idx += 1
 
@@ -305,8 +323,12 @@ class G1RoughDemo:
             if self._selected_id:
                 self.commands[self._selected_id] = self._key_to_control["ZEROS"]
 
+    '''
+    ROBOT MOVEMENT / VELOCITY INSTRUCTIONS
+    '''
 
     def move_robot_for_duration(self, cur_pose, new_pose, agent_id, duration_steps=60):
+        """Tried using duration steps to time robot movement"""
         env_idx = self.agent_to_env[agent_id]
         # Initialize tracking dictionary if needed
         if not hasattr(self, "agent_move_counters"):
@@ -334,7 +356,6 @@ class G1RoughDemo:
         desired_yaw = math.atan2(dy, dx)
         dyaw = (desired_yaw - cur_yaw + math.pi) % (2 * math.pi) - math.pi
 
-        # Proportional control
         k_linear = 1.0
         k_angular = 1.0
         linear_velocity = k_linear * distance
@@ -342,7 +363,6 @@ class G1RoughDemo:
         print(f"linear_velocity: {linear_velocity}")
         print(f"angular_velocity: {angular_velocity}")
 
-        # Clamp max velocities
         max_lin = 1.0
         max_ang = 0.75
         linear_velocity = max(-max_lin, min(linear_velocity, max_lin))
@@ -352,15 +372,17 @@ class G1RoughDemo:
         print(f"linear_velocity: {linear_velocity}")
         print(f"angular_velocity: {angular_velocity}")
 
+        """For half the steps I wanted the robot to have angular velocity and then the other half focus on 
+        moving forward since angular velocity often cause robot to rotate and not move forward"""
         if self.agent_move_counters[agent_id] < duration_steps / 2:
-            # Send velocity command
+            #send velocity command
             self.commands[env_idx] = torch.tensor(
                 [linear_velocity, 0.0, 0.0, angular_velocity],
                 device=self.device
             )
             self.agent_move_counters[agent_id] += 1
         elif self.agent_move_counters[agent_id] < duration_steps:
-            # Send velocity command
+            #send velocity command
             self.commands[env_idx] = torch.tensor(
                 [linear_velocity, 0.0, 0.0, 0],
                 device=self.device
@@ -379,6 +401,7 @@ class G1RoughDemo:
 
         self.agents[agent_id].pose = new_pose
 
+    """Doesn't work as intended but supposed to get current robot pose realtime in sim"""
     def get_robot_pose_in_isaac(self, env_idx):
         robot_prim_path = f"/World/envs/env_{env_idx}"
 
@@ -391,11 +414,11 @@ class G1RoughDemo:
         xform = UsdGeom.Xform(robot_prim)
         transform_matrix = xform.GetLocalTransformation()
 
-        # Extract translation from the matrix
+        #get translation from the matrix
         translation = transform_matrix.ExtractTranslation()
         x, y, z = translation[0], translation[1], translation[2]
 
-        # Extract rotation matrix and compute yaw
+        #get rotation matrix and compute yaw
         rotation_matrix = transform_matrix.ExtractRotation()
         forward = rotation_matrix.TransformDir(Gf.Vec3d(1, 0, 0))
         yaw = np.arctan2(forward[1], forward[0])
@@ -430,8 +453,6 @@ class G1RoughDemo:
         if self._previous_selected_id is not None and self._previous_selected_id != self._selected_id:
             self.env.unwrapped.command_manager.reset([self._previous_selected_id])
             self.commands[:, 0:3] = self.env.unwrapped.command_manager.get_command("base_velocity")
-
-
     
     def _update_camera(self):
         """Updates the per-frame transform of the third-person view camera to follow
@@ -450,16 +471,34 @@ class G1RoughDemo:
 
 def main():
     """Main function."""
-    demo_g1 = G1RoughDemo()
+    demo_g1 = G1MrNavDemo()
     obs, _ = demo_g1.env.reset()
     demo_g1.set_agent_spawns(demo_g1.spawn_pos)
     demo_g1.convert_movable_obstacles()
     demo_g1.history = demo_g1.sim.run()
+    """Currently runs simulation after 2D simulation, using sim step results for each sim step,
+    Ideally want the steps to be happening real time and not from results but more complex to integrate and I wanted
+    to focus on getting robot movement correct first """
+    while simulation_app.is_running():
+        for step in demo_g1.history:
+            action_results = step.action_results
+            for agent_id, action_result in action_results.items():
+                env_index = demo_g1.agent_to_env.get(agent_id)
 
-    while simulation_app.is_running():  
-
+                if env_index is None:
+                    raise KeyError(f"Unknown agent_id '{agent_id}' in results")
+                
+                if isinstance(action_result, ar.ActionSuccess):
+                    new_pose = action_result.robot_pose
+                    cur_pose = demo_g1.agents[agent_id].pose
+                    demo_g1.move_robot_for_duration(cur_pose, new_pose, agent_id, duration_steps=60)
+                    # now you can use robot_pose + env_index however you like
+                    print(f"[env {env_index}] {agent_id} pose = {new_pose}")
+                else:
+                    # it's some other ActionResult (e.g. a failure or no-op), skip or handle differently
+                    print(f"[env {env_index}] {agent_id} did not succeed, skipping pose extraction")
         # check for selected robots
-        #demo_g1.update_selected_object()
+        demo_g1.update_selected_object()
         with torch.inference_mode():
             action = demo_g1.policy(obs)
             obs, _, _, _ = demo_g1.env.step(action)
